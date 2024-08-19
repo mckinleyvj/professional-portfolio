f1828617267061c64dde7361a33869a0fc45e254
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2024-08-19 11:25:46 +1000

    update modal

--- assets/js/sanity.js
+++ assets/js/sanity.js
@@ -248,31 +248,15 @@ async function fetchSanityDocument() {
                       // Get the <span> element that closes the modal
                       const span = document.getElementsByClassName("close")[0];
                     
-                      // Get the buttons
-                      const confirmBtn = document.getElementById("confirmRedirect");
-                      const cancelBtn = document.getElementById("cancelRedirect");
-                    
-                      // When the user clicks on <span> (x), close the modal
+                      // Close the modal when the close button is clicked
                       span.onclick = function() {
                         modal.style.display = "none";
                       };
                     
-                      // When the user clicks "Yes", redirect to the src
-                      confirmBtn.onclick = function() {
+                      // Redirect to the provided URL after displaying the modal
+                      setTimeout(() => {
                         window.location.href = theSrc;
-                      };
-                    
-                      // When the user clicks "No", close the modal
-                      cancelBtn.onclick = function() {
-                        modal.style.display = "none";
-                      };
-                    
-                      // When the user clicks anywhere outside of the modal, close it
-                      window.onclick = function(event) {
-                        if (event.target == modal) {
-                          modal.style.display = "none";
-                        }
-                      };
+                      }, 1000); // Wait for 1 second before redirecting
                     }
 
                     storylineBtn.addEventListener("click", function() {
