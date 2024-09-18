6d39df5d7a88f94811d7fbb91067c23b0fde2f75
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2024-09-18 16:03:42 +1000

    decrease font from 16 to 14. Set table header align left. then sort by language and title

--- assets/js/resource.js
+++ assets/js/resource.js
@@ -8,7 +8,8 @@ async function fetchSanityResourceList() {
     const aTc = atob(tc); 
             
     //const query = `*[_type == 'topic' && __i18n_lang == 'en'] | order(title) {_type,title,resource_type,__i18n_lang,_createdAt,_updatedAt}`;
-    const query = `*[_type == 'topic' && __i18n_lang == 'en' && !(_id in path("drafts.**"))] | order(title) {_id,_type,title,resource_type,__i18n_lang,_createdAt,_updatedAt}`;
+    //const query = `*[_type == 'topic' && __i18n_lang == 'en' && !(_id in path("drafts.**"))] | order(title) {_id,_type,title,resource_type,__i18n_lang,_createdAt,_updatedAt}`;
+    const query = `*[_type == 'topic' && !(_id in path("drafts.**"))] | order(__i18n_lang asc, updatedAt asc) {_id,_type,title,resource_type,__i18n_lang,_createdAt,_updatedAt}`;
 
     //const resourceDoc = `https://${prjId}.api.sanity.io/v1/data/query/${ds}?query=*[_id == "${docId}"]`;
     const resourceDoc = `https://${prjId}.api.sanity.io/v1/data/query/${ds}?query=${encodeURIComponent(query)}`;
