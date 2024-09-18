efde7e350654c4fac30579b12d534b88a966ec0c
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2024-09-18 16:46:04 +1000

    removed max width and set ordering by last update

--- assets/js/resource.js
+++ assets/js/resource.js
@@ -9,7 +9,8 @@ async function fetchSanityResourceList() {
             
     //const query = `*[_type == 'topic' && __i18n_lang == 'en'] | order(title) {_type,title,resource_type,__i18n_lang,_createdAt,_updatedAt}`;
     //const query = `*[_type == 'topic' && __i18n_lang == 'en' && !(_id in path("drafts.**"))] | order(title) {_id,_type,title,resource_type,__i18n_lang,_createdAt,_updatedAt}`;
-    const query = `*[_type == 'topic' && !(_id in path("drafts.**"))] | order(__i18n_lang asc, _updatedAt desc) {_id,_type,title,resource_type,__i18n_lang,_createdAt,_updatedAt}`;
+    //const query = `*[_type == 'topic' && !(_id in path("drafts.**"))] | order(__i18n_lang asc, _updatedAt desc) {_id,_type,title,resource_type,__i18n_lang,_createdAt,_updatedAt}`;
+    const query = `*[_type == 'topic' && !(_id in path("drafts.**"))] | order(_updatedAt desc) {_id,_type,title,resource_type,__i18n_lang,_createdAt,_updatedAt}`;
 
     //const resourceDoc = `https://${prjId}.api.sanity.io/v1/data/query/${ds}?query=*[_id == "${docId}"]`;
     const resourceDoc = `https://${prjId}.api.sanity.io/v1/data/query/${ds}?query=${encodeURIComponent(query)}`;
