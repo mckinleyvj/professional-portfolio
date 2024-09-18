c622f5349b98554b610c50bbcc2a4e7d1724fda0
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2024-09-18 14:40:42 +1000

    Added contentindex page to load resourcelist via API

--- assets/js/sanity.js
+++ assets/js/sanity.js
@@ -372,3 +372,40 @@ async function fetchSanityDocument() {
     }
 
 fetchSanityDocument();
+
+async function fetchSanityResourceList() {
+  const pid = "Mmk4MGNrMDE=";
+  const dts = "cHJvZHVjdGlvbg==";
+  const tc = "c2tYYVVIcUpFNVdhdzFPeExXM0Y1TlM4SFUyQmM0bEhaN0F4ZWNWeldiUGhUa2RFRDhEYVlVdXlKY05sMHZyNnpMajBYWUMyRjIwODBzOW12aVZTSmE0SWNvem96SWF3emg5TmtCQXlOdjQweXlaR0tCb3JDRG5udmF4RXRjcGdzVU4wSUtWd29TcktDVTJ4cldBMjhGUTVVemx4eVBVZW9BTEttc3hqdzdtN3FmMXRTcWt6"; 
+  
+  const prjId = atob(pid);
+  const ds = atob(dts);
+  const aTc = atob(tc); 
+          
+  const query = '*[_type == "Resources"]';
+
+  //const resourceDoc = `https://${prjId}.api.sanity.io/v1/data/query/${ds}?query=*[_id == "${docId}"]`;
+  const resourceDoc = `https://${prjId}.api.sanity.io/v1/data/query/${ds}?query=${encodeURIComponent(query)}`;
+
+  try {
+   // Make the API call
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Documents:', data.result);
+    })
+    .catch(error => {
+      console.error('Error fetching documents:', error);
+    });
+  } catch (error) {
+    console.error("Error fetching Sanity document:", error);
+    document.getElementById("content").innerText = error.message;
+  }
+}
+
+fetchSanityResourceList();
